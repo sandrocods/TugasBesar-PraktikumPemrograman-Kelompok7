fe2ece4392a257f06e228a2aff06c57f9f4e3afb
sandrocods <59155826+sandrocods@users.noreply.github.com>
2022-07-08 09:06:38 +0700

    update error

--- helpers/handlers.py
+++ helpers/handlers.py
@@ -1,3 +1,14 @@
+##
+# Author  : Sandroputraa
+# Name    : Igracias LMS ITTP Telegram Bot
+# Build   : 08-07-2022
+#
+# If you are a reliable programmer or the best developer, please don't change anything.
+# If you want to be appreciated by others, then don't change anything in this script.
+# Please respect me for making this tool from the beginning.
+##
+
+
 import os
 import re
 import schedule
